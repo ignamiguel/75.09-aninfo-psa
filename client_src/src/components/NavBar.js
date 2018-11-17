50c867d320c950dd8a83a1a7a45aabfaf72ebe2c
Gaston Tulipani <gtulipani@hotmail.com>
2018-11-17 17:10:48 -0300

    Added Frontend in ReactJS already integrated with the BE. Build instructions have also been included in the README file

---
+++ client_src/src/components/NavBar.js
@@ -0,0 +1,151 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TimerIcon from '@material-ui/icons/AvTimer';
+import HelpIcon from '@material-ui/icons/Help'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import NavBarOption from './NavBarOption';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginLeft: 12,
+      marginRight: 20,
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3,
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  });
+
+class NavBar extends Component{
+    state = {
+        open: false
+      };
+    
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+    
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
+
+    render(){
+        const { classes, theme } = this.props;
+        const { open } = this.state;
+
+        return (
+            <div>
+            <AppBar position="static" className={classNames(classes.appBar, {
+                [classes.appBarShift]: open,
+            })}>
+                <Toolbar disableGutters={!open}>
+                    <IconButton 
+                        color="inherit"
+                        aria-label="Menu"
+                        onClick={this.handleDrawerOpen}
+                        className={classNames(classes.menuButton, open && classes.hide)}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="title" color="inherit" noWrap>
+                        PSA - Time Tracker
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={this.handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                    <ListItem button key='Close Menu' onClick={this.handleDrawerClose}>
+                        <ListItemText primary='Close Menu' />
+                    </ListItem>
+                </div>
+                <Divider />
+                    <List>
+                        <NavBarOption text='Hours' icon= {<TimerIcon />} link="/"/>
+                    </List>
+                <Divider />
+                    <List>
+                        <NavBarOption text='About' icon= {<HelpIcon />} link="/about"/>
+                    </List>
+            </Drawer>
+            </div>
+        )
+    }
+}
+
+NavBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+  };
+  
+export default withStyles(styles, { withTheme: true })(NavBar);
