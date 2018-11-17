000c1f40d26f17b5a81baacd67970fc65e14bf43
Gaston Tulipani <gtulipani@hotmail.com>
2018-11-17 17:18:47 -0300

    Added SQL migration scripts and description in README

--- README.md
+++ README.md
@@ -13,6 +13,14 @@ npm run-script build
 ```
 This command will build the React Application and copy the `build` folder to the client folder from the higher level.
 Alternately, the same command can be executed from the root path, and it will move to the `client_src` directory, run the same command and get back to the root path.
+
+### Start MySQL
+The Application uses MySQL as datasource. A database called `timetracker` must be created before running the application among with the following tables:
+- Worker
+- Task
+- Hour
+A migration script that creates the tables is available under the `/resources` folder.
+
 ### Run Application
 In order to run the application, the following command must be executed: `node .`
 #### API
---
+++ resources/migration/01_tables_creation.sql
@@ -0,0 +1,29 @@
+CREATE TABLE timetracker.Hour
+(
+    id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    quantity int(11) NOT NULL,
+    date datetime NOT NULL,
+    worker_id int(11) NOT NULL
+);
+
+CREATE TABLE timetracker.Task
+(
+    id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    description text,
+    type varchar(20) NOT NULL,
+    status varchar(15) NOT NULL,
+    begin_date datetime,
+    end_date datetime,
+    hour_ids text,
+    assigned_worker_id int(11) NOT NULL,
+    CONSTRAINT Task_Worker_id_fk FOREIGN KEY (assigned_worker_id) REFERENCES timetracker.Worker (id)
+);
+CREATE INDEX Task_Worker_id_fk ON timetracker.Task (assigned_worker_id);
+
+CREATE TABLE timetracker.Worker
+(
+    id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL
+);
